67c800138cd22a0591c57795e5e98f8044bf50c1
Wilfred <wilfred05777@gmail.com>
2022-06-17 08:17:53 +0800

    Product Schema

---
+++ api/app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,77 @@
+<?php
+// php artisan make:controller Api/ProductController --api
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = new Product();
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+
+        $product->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+      $product = Product::findOrFail($request->id);
+      $product->description = $request->description;
+      $product->price = $request->price;
+      $product->stock = $request->stock;
+
+      $product->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+       $product = Product::destroy($id);
+       return $product;
+    }
+}
--- api/database/migrations/2022_06_16_234039_create_products_table.php
+++ api/database/migrations/2022_06_16_234039_create_products_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('description');
+            $table->double('price', 8, 2);
+            $table->integer('stock');
             $table->timestamps();
         });
     }
