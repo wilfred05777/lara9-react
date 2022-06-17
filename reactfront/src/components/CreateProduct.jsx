2da1db399d6d5cd22cf528d7c0246cdf08d36b6b
Wilfred <wilfred05777@gmail.com>
2022-06-17 10:30:51 +0800

    Component Create Product

--- reactfront/src/components/CreateProduct.jsx
+++ reactfront/src/components/CreateProduct.jsx
@@ -1,7 +1,64 @@
 import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const endpoint = "http://localhost:8000/api/product";
 
 const CreateProduct = () => {
-  return <div>CreateProduct</div>;
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [stock, setStock] = useState("");
+
+  const navigate = useNavigate();
+
+  const store = async (e) => {
+    e.preventDefault();
+    await axios.post(endpoint, {
+      description: description,
+      price: price,
+      stock: stock,
+    });
+    navigate("/");
+  };
+
+  return (
+    <div className="container">
+      <h3>Create Product</h3>
+      <form onSubmit={store}>
+        <div className="mb-3">
+          <label className="form-label">Description</label>
+          <input
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Price</label>
+          <input
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            type="number"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Stock</label>
+          <input
+            value={stock}
+            onChange={(e) => setStock(e.target.value)}
+            type="number"
+            className="form-control"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Store
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateProduct;
