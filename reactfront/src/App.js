b908802c8fba658412876f1a73fdb4f1d7ab5596
Wilfred <wilfred05777@gmail.com>
2022-06-17 11:17:54 +0800

    toastify bug fixed and working

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -1,30 +1,31 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import CreateProduct from "./components/CreateProduct";
 import EditProduct from "./components/EditProduct";
 import ShowProducts from "./components/ShowProducts";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
-      <Router>
+      {/* <Router>
         <Routes>
           <Route exact path="/" element={<ShowProducts />} />
           <Route exact path="/create" element={<CreateProduct />} />
           <Route exact path="/edit/:id" element={<EditProduct />} />
         </Routes>
-      </Router>
+      </Router> */}
       <ToastContainer />
       {/* <button className="btn btn-primary">Create</button> */}
-      {/* <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<ShowProducts />} />
-            <Route exact path="/create" element={<CreateProduct />} />
-            <Route exact path="/edit/:id" element={<EditProduct />} />
-          </Routes>
-        </BrowserRouter> */}
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<ShowProducts />} />
+          <Route exact path="/create" element={<CreateProduct />} />
+          <Route exact path="/edit/:id" element={<EditProduct />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
